10d41b525131dac9600b07a698c56ef2f2e9d2d6
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-04 10:24:57 +0100

    search function gemaakt

--- app/Http/Controllers/SearchUsers.php
+++ app/Http/Controllers/SearchUsers.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SearchUsers extends Controller
 {
-    //
+    public $search = '';
+ 
+    public function render()
+    {
+        return view('livewire.search-users', [
+            'users' => User::where('name', $this->search)->get(),
+        ]);
+    }
+
 }
