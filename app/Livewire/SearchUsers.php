cf75094bdfa6ee36255e7e73d29fdb428cb844f3
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-08 12:56:44 +0100

    Refactor search functionality and improve UI

--- app/Livewire/SearchUsers.php
+++ app/Livewire/SearchUsers.php
@@ -8,11 +8,18 @@ use Livewire\Component;
 class SearchUsers extends Component
 {
     public $search = '';
- 
+
     public function render()
     {
+        $users = User::where('name', 'like', '%' . $this->search . '%')->get();
+
         return view('livewire.search-users', [
-            'users' => User::where('name', $this->search)->get(),
+            'users' => $users,
         ]);
+    }
+
+    public function submit()
+    {
+        // This method doesn't need to do anything. It's just here to cause Livewire to refresh the component when the form is submitted.
     }
 }
--- resources/views/livewire/search-users.blade.php
+++ resources/views/livewire/search-users.blade.php
@@ -1,9 +1,11 @@
-<div>
-    <input wire:model="search" type="text" placeholder="Search users..."/>
- 
-    <ul>
+<div class="p-6">
+    <form wire:submit.prevent="submit">
+        <input wire:model.debounce.500ms="search" type="text" placeholder="Search users..." class="px-4 py-2 border rounded" />
+    </form>
+
+    <ul class="mt-4 space-y-2">
         @foreach($users as $user)
-            <li>{{ $user->name }}</li>
+        <li class="px-4 py-2 border rounded">{{ $user->name }}</li>
         @endforeach
     </ul>
 </div>
