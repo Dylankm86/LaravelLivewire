ce16e2000f735c6a0cdea1c2734bc5393a7d76a1
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-08 13:43:04 +0100

    Add event listener for UserUpdated event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \app\Events\UserUpdated::class => [
+            \app\Listeners\SendUserUpdatedEmail::class,
+        ],
     ];
 
     /**
